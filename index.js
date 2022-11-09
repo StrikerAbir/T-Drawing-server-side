260a66b74bba57202bfc0eb334e461b47a28c43c
Abir Hasan <cse.170201063@gmail.com>
2022-11-09 11:41:04 +0600

    api for post a single review created

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 1000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-require('dotenv').config();
+require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 // middleware
@@ -12,7 +12,7 @@ app.use(express.json());
 
 // env variable datas
 const user = process.env.DB_USER;
-const password = process.env.PASS
+const password = process.env.PASS;
 const Secret = process.env.ACCESS_TOKEN_SECRET;
 
 // mongodb and api
@@ -41,61 +41,79 @@ function verifyJWT(req, res, next) {
 
 async function run() {
   try {
-      const servicesCollection = client.db('tDrawing').collection('services')
-      
-      //jwt api
-      app.post("/jwt", (req, res) => {
-        const user = req.body;
-        console.log(user);
-        const token = jwt.sign(user, Secret, { expiresIn: "1000d" });
-        res.send({ token });
-      });
+    const servicesCollection = client.db("tDrawing").collection("services");
+    const reviewsCollection = client.db("tDrawing").collection("reviews");
 
-    //* all services api
-      
+    //jwt api
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, Secret, { expiresIn: "1000d" });
+      res.send({ token });
+    });
+
+    //  * all services api
+
     //   get services data
-      app.get("/services", async (req, res) => {
-        const query = {};
-        const count = parseInt(req.query.count);
-        if (count) {
-          const cursor = servicesCollection.find(query);
-          const services = await cursor.limit(count).toArray();
-          res.send(services);
-        } else {
-          const cursor = servicesCollection.find(query);
-          const services = await cursor.toArray();
-          res.send(services);
-          }
-          
-          // get a single service
-          app.get("/services/:id", async (req, res) => {
-              const id = req.params.id;
-              const query = { _id: ObjectId(id) };
-              const service = await servicesCollection.findOne(query);
-            //   console.log(service);
-              res.send(service);
-          });
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const count = parseInt(req.query.count);
+      if (count) {
+        const cursor = servicesCollection.find(query);
+        const services = await cursor.limit(count).toArray();
+        res.send(services);
+      } else {
+        const cursor = servicesCollection.find(query);
+        const services = await cursor.toArray();
+        res.send(services);
+      }
+    });
 
-        //   post a single service
-        app.post("/services", verifyJWT, async (req, res) => {
-          const service = req.body;
-          console.log(service);
-          const result = await servicesCollection.insertOne(service);
-          res.send(result);
-        });
-      });
+    // get a single service
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await servicesCollection.findOne(query);
+      //   console.log(service);
+      res.send(service);
+    });
 
-    
+    //   post a single service
+    app.post("/services", verifyJWT, async (req, res) => {
+      const service = req.body;
+      console.log(service);
+      const result = await servicesCollection.insertOne(service);
+      res.send(result);
+    });
+
+    // *all reviews api
+      
+    //   get all reviews
+    app.get("/reviews", async (req, res) => {
+      const query = {};
+      const cursor = reviewsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+      
+    // post a single review
+      app.post("/reviews", async (req, res) => {
+           const review = req.body;
+           console.log(review);
+           const result = await reviewsCollection.insertOne(review);
+           res.send(result);
+      })
+      
+    //   -----------------------------------    //
   } finally {
   }
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send('T-Drawing server running...');
-})
+app.get("/", (req, res) => {
+  res.send("T-Drawing server running...");
+});
 
 app.listen(port, () => {
-    console.log(`T-Drawing Server running on port ${port}`);
-})
+  console.log(`T-Drawing Server running on port ${port}`);
+});
